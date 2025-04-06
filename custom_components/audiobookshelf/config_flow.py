6583126a58d84d31ba62386a2644487b50976b3e
Bren Dahl <dahl.brendan@gmail.com>
2025-04-06 17:31:45 -0400

    feat: add service to remove media progress keyed to series name

--- custom_components/audiobookshelf/config_flow.py
+++ custom_components/audiobookshelf/config_flow.py
@@ -4,13 +4,14 @@ from __future__ import annotations
 
 import logging
 
-import aiohttp
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
+from aiohttp import ClientSession
 from homeassistant import config_entries
-from homeassistant.const import CONF_API_KEY, CONF_SCAN_INTERVAL, CONF_URL
+from homeassistant.const import CONF_SCAN_INTERVAL, CONF_URL, CONF_TOKEN
+from aioaudiobookshelf import SessionConfiguration, LoginError, get_user_client_by_token
 
-from .const import DOMAIN, HTTP_AUTH_FAILURE, HTTP_OK
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -18,10 +19,10 @@ _LOGGER = logging.getLogger(__name__)
 def validate_config(data: dict[str, str]) -> dict:
     """Validate the config entries."""
     errors = {}
-    if not bool(data[CONF_API_KEY]):
-        errors[CONF_API_KEY] = "api_key_invalid"
     if not bool(data[CONF_URL]):
         errors[CONF_URL] = "url_invalid"
+    if CONF_TOKEN not in data:
+        errors[CONF_URL] = "api_key_invalid"
     if not data[CONF_URL].startswith(("http://", "https://")):
         errors[CONF_URL] = "url_protocol_missing"
     if not bool(data[CONF_SCAN_INTERVAL]):
@@ -29,37 +30,32 @@ def validate_config(data: dict[str, str]) -> dict:
     return errors
 
 
-async def verify_config(data: dict) -> dict:
+async def verify_config(data: dict[str, str]) -> dict:
     """Verify the configuration by testing the API connection."""
-    async with aiohttp.ClientSession() as session:
-        headers = {"Authorization": f"Bearer {data[CONF_API_KEY]}"}
-        try:
-            async with session.get(
-                f"{data[CONF_URL]}/api/libraries",
-                headers=headers,
-                timeout=aiohttp.ClientTimeout(5),
-            ) as response:
-                if response.status != HTTP_OK:
-                    msg = f"Failed to connect to API: {response.status}"
-                    _LOGGER.error("%s", msg)
-                    if response.status == HTTP_AUTH_FAILURE:
-                        return {"base": "api_auth_error"}
-                    return {"base": "api_other_error"}
-                return {}
-        except TimeoutError:
-            return {"base": "api_timeout_error"}
-        except aiohttp.ClientConnectorError:
-            return {"base": "api_client_connector_error"}
+    try:
+        async with ClientSession() as session:
+            await get_user_client_by_token(
+                session_config=SessionConfiguration(
+                    session=session,
+                    url=data[CONF_URL],
+                    token=data[CONF_TOKEN],
+                    logger=_LOGGER,
+                    pagination_items_per_page=30,
+                ),
+            )
+        return {}
+    except LoginError:
+        return {"base": "api_auth_error"}
 
 
 class AudiobookshelfConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Audiobookshelf."""
 
-    VERSION = 1
+    VERSION = 2
 
     async def async_step_user(
         self, user_input: dict[str, str] | None = None
-    ) -> config_entries.ConfigFlowResult:
+    ):
         """Handle the user step."""
         errors = {}
 
@@ -76,7 +72,7 @@ class AudiobookshelfConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                                 CONF_URL, default=user_input.get(CONF_URL, "")
                             ): str,
                             vol.Required(
-                                CONF_API_KEY, default=user_input.get(CONF_API_KEY, "")
+                                CONF_TOKEN, default=user_input.get(CONF_TOKEN, "")
                             ): str,
                             vol.Optional(
                                 CONF_SCAN_INTERVAL,
@@ -89,9 +85,10 @@ class AudiobookshelfConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
             return self.async_create_entry(
                 title="Audiobookshelf",
+                unique_id="Audiobookshelf",
                 data={
                     CONF_URL: user_input[CONF_URL],
-                    CONF_API_KEY: user_input[CONF_API_KEY],
+                    CONF_TOKEN: user_input[CONF_TOKEN],
                     CONF_SCAN_INTERVAL: user_input.get(CONF_SCAN_INTERVAL, 300),
                 },
             )
@@ -102,7 +99,7 @@ class AudiobookshelfConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_URL): str,
-                    vol.Required(CONF_API_KEY): str,
+                    vol.Required(CONF_TOKEN): str,
                     vol.Optional(CONF_SCAN_INTERVAL, default=300): cv.positive_int,
                 }
             ),
